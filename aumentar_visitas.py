65de8d362cb87f8c77bc76dad363d8696b44cd22
flashtheleader <gonzaleema95@gmail.com>
2026-08-05 19:31:03 -0300

    Update aumentar_visitas.py

--- aumentar_visitas.py
+++ aumentar_visitas.py
@@ -5,16 +5,16 @@ import time;
 from selenium import webdriver;
 
 #Tiempo para refrescas la página
-Timer = 5
+Timer = 60
 
 #Enlace (Blog, YouTube)
-enlace = 'https://www.youtube.com/watch?v=Auzs95InJzo'
+enlace = 'https://www.youtube.com/watch?v=LMR6lCAQU-Y'
 
 #Número visitas
 views = 1000
 
 #driver
-driver = webdriver.Chrome("C:/gisbook/chromedriver.exe")
+driver = webdriver.Chrome("C:\Users\flash\OneDrive\Desktop\chromedriver-win64")
 driver.get(enlace)
 
 for i in range(views):
